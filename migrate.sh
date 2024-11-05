f30bd3aff794d1237e862e9a381677663381c38f
V2as <115551035+V2as@users.noreply.github.com>
2024-11-05 22:42:15 +0300

    Update migrate.sh
    + service feature

--- migrate.sh
+++ migrate.sh
@@ -35,19 +35,48 @@ docker cp amnezia-awg:/opt/amnezia/awg/wireguard_psk.key /opt/amnezia/awg
 docker cp amnezia-awg:/opt/amnezia/awg/wg0.conf /opt/amnezia/awg
 
 
-CONFIG_FILE="/opt/amnezia/awg/wg0.conf"
-NEW_CONFIG_FILE="/opt/amnezia/awg/wg0.conf"
+#CONFIG_FILE="/opt/amnezia/awg/wg0.conf"
+#NEW_CONFIG_FILE="/opt/amnezia/awg/wg0.conf"
 
-MASK=$(grep -oP 'Address\s*=\s*\K[^\s]+' $CONFIG_FILE)
+#MASK=$(grep -oP 'Address\s*=\s*\K[^\s]+' $CONFIG_FILE)
+#
+#PORT=$(grep -oP 'ListenPort\s*=\s*\K\d+' $CONFIG_FILE)
 
-PORT=$(grep -oP 'ListenPort\s*=\s*\K\d+' $CONFIG_FILE)
 
 
-
-iptables -t nat -A POSTROUTING -s $MASK -o ens3 -j MASQUERADE; iptables -A INPUT -p udp -m udp --dport $PORT -j ACCEPT; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT;
+#iptables -t nat -A POSTROUTING -s $MASK -o ens3 -j MASQUERADE; iptables -A INPUT -p udp -m udp --dport $PORT -j ACCEPT; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT;
 #iptables -t nat -D POSTROUTING -s $MASK -o ens3 -j MASQUERADE; iptables -D INPUT -p udp -m udp --dport $PORT -j ACCEPT; iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT;
-iptables-save -t nat
+
 docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
 
-wg-quick up /opt/amnezia/awg/wg0.conf
+#wg-quick up /opt/amnezia/awg/wg0.conf
+
+
+CONFIG_FILE="/opt/amnezia/awg/wg0.conf"
+SERVICE_FILE="/etc/systemd/system/black.service"
+
+MASK=$(grep -oP 'Address\s*=\s*\K[^\s]+' $CONFIG_FILE)
+PORT=$(grep -oP 'ListenPort\s*=\s*\K\d+' $CONFIG_FILE)
+
+IPTABLES_RULES="iptables -t nat -A POSTROUTING -s $MASK -o ens3 -j MASQUERADE; iptables -A INPUT -p udp -m udp --dport $PORT -j ACCEPT; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT;"
+
+echo "[Unit]
+Description=WireGuard via wg-quick
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/wg-quick up /opt/amnezia/awg/wg0.conf
+ExecStartPost=/bin/bash -c '$IPTABLES_RULES'
+ExecStop=/usr/bin/wg-quick down /opt/amnezia/awg/wg0.conf
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target" > $SERVICE_FILE
+
+systemctl daemon-reload
+
+systemctl enable black.service
+systemctl start black.service
+iptables-save -t nat
