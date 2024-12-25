81a90b4b928f35ea2476ddfcd7d1698d61c320be
V2as <115551035+V2as@users.noreply.github.com>
2024-12-25 05:24:20 +0300

    Update installWarp.sh

--- installWarp.sh
+++ installWarp.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-apt-get update && apt-get install cloudflare-warp --accept-tos flag
+apt-get update && apt-get install cloudflare-warp
 #   
 warp-cli registration new
 warp-cli mode proxy
