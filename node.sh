b622ac07126cfd2fc2fde7f0c6f631a06deca3f1
V2as <115551035+V2as@users.noreply.github.com>
2025-02-27 22:51:09 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -173,5 +173,11 @@ warp-cli --accept-tos connect
 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.ens3.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 
