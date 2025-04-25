77ed4370e46143ab7b4174d4be6aa56a3405c7b8
V2as <115551035+V2as@users.noreply.github.com>
2025-04-25 18:17:05 +0300

    Update selfsteal.sh

--- selfsteal.sh
+++ selfsteal.sh
@@ -182,7 +182,7 @@ sudo docker compose -f $DOCKER_COMPOSE_PATH up -d
 
 
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ noble main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 apt-get update && apt-get install cloudflare-warp -y
 warp-cli --accept-tos registration new
 warp-cli --accept-tos mode proxy
